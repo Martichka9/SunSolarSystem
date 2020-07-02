e3a889e35dc4118c3f72ef8f50b5edf8ab58a667
Martichka9 <Martichka9@users.noreply.github.com>
2020-07-02 17:22:11 +0300

    Change sizes

--- src/objects/sun.js
+++ src/objects/sun.js
@@ -11,27 +11,24 @@ function addLight( h, s, l, x, y, z ) {
     var textureFlare0 = textureLoader.load( img0 );
     var textureFlare3 = textureLoader.load( img3 );
 
-    var light = new THREE.PointLight( 0xff0000, 2, 0 );
+    var light = new THREE.PointLight( 0xff0000, 2, 5000 );
     light.color.setHSL( h, s, l );
     light.position.set( x, y, z );
 
     var lensflare = new Lensflare();
-    lensflare.addElement( new LensflareElement( textureFlare0, 1000, 0, light.color ) );
+    lensflare.addElement( new LensflareElement( textureFlare0, 1090, 0, light.color ) );
     lensflare.addElement( new LensflareElement( textureFlare3, 800, 0.6 ) );
-    lensflare.addElement( new LensflareElement( textureFlare3, 1100, 0.7 ) );
+    lensflare.addElement( new LensflareElement( textureFlare3, 1190, 0.7 ) );
     lensflare.addElement( new LensflareElement( textureFlare3, 600, 0.9 ) );
-    lensflare.addElement( new LensflareElement( textureFlare3, 400, 1 ) );
+    lensflare.addElement( new LensflareElement( textureFlare3, 300, 1 ) );
     light.add( lensflare );
 
     return light;
 }
 
-
 var sunlight = addLight( 0.08, 0.8, 0.5, 0, 0, 0 );
 
 //sun
-var sun = buildARock(5,'star','0xffae42',false,false,0,0,0);
-
-
+var sun = buildARock(1090,'star','0xffae42',false,false,0,0,0);
 
 export {sunlight,sun};
