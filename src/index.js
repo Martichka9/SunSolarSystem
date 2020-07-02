9395b483083b1b6132112cb9c77dc0c73af73188
Martichka9 <Martichka9@users.noreply.github.com>
2020-07-02 17:15:43 +0300

    Change camera position and perspective,  include planets, include OrbitControls

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import * as THREE from 'three';
 import {sunlight, sun} from './objects/sun.js';
+import * as PLANETS from './objects/planets.js';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+//body and canvas styles set
+document.getElementsByTagName('body')[0].style.margin = 0;
+document.getElementsByTagName('body')[0].style.overflow = 'hidden';
 
 //main vars
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-camera.position.z = 100;
+var camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+camera.position.set(5000,5000,5000);
+camera.aspect = window.innerWidth / window.innerHeight;
 
 var light = new THREE.AmbientLight( 0x909009 ); // soft white light
 scene.add( light );
@@ -13,13 +20,30 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var axesHelper = new THREE.AxesHelper( 6000 );
+scene.add( axesHelper );
+var controls = new OrbitControls( camera, renderer.domElement );
+
 //sun
-//scene.add(sun);
+// scene.add(sun);
 scene.add( sunlight );
+
+//planets
+scene.add(PLANETS.mercury,
+          PLANETS.venus,
+          PLANETS.earth,
+          PLANETS.mars,
+          PLANETS.jupiter,
+          PLANETS.saturn,
+          PLANETS.uranus,
+          PLANETS.neptune,
+          PLANETS.pluto);
 
 var animate = function () {
     requestAnimationFrame( animate );
 
+    PLANETS.earth.rotation.set(0.001,0.01,0.1);
+    controls.update();
     renderer.render( scene, camera );
 };
 
