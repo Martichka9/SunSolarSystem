3746ef63ddab1fb46d0bb6755d4dd78695ba926d
Martichka9 <Martichka9@users.noreply.github.com>
2020-07-03 14:53:05 +0300

    Added function to apply texture to planets

--- src/objects/build-a-rock.js
+++ src/objects/build-a-rock.js
@@ -1,13 +1,19 @@
+import sRings from '../imgs/552-5527537_-mr2.png';
+import { TextureLoader } from 'three';
+
 //function to build the system elements
-export function buildARock (size,type,color,cShadow,rShadow,x,y,z,haveRings){
+export function buildARock (size,type,color,cShadow,rShadow,x,y,z,haveRings, texture){
+// export function buildARock (size,type,color,cShadow,rShadow,x,y,z,haveRings){
     let geo, mater, rock, rings; 
     geo = new THREE.SphereBufferGeometry(size,32,32);
 
     if(type === 'star'){
-        mater = new THREE.MeshPhysicalMaterial( {color: color, transparent: true, opacity: 0.5} )
+        mater = addTexture(texture);
+        //mater = new THREE.MeshPhysicalMaterial( {color: color, transparent: true, opacity: 0.5} )
     }
     else if(type === 'planet'){
-        mater = new THREE.MeshLambertMaterial( {color: color} )
+        mater = addTexture(texture);
+        //mater = new THREE.MeshLambertMaterial( {color: color} )
         if(haveRings === 1){
             rings = addRings(size+6,size*2);
         }
@@ -21,7 +27,6 @@ export function buildARock (size,type,color,cShadow,rShadow,x,y,z,haveRings){
         let planetWithRings = new THREE.Object3D();
         planetWithRings.add(rock, rings);
         planetWithRings.position.set(x,y,z);
-        planetWithRings.rotateX(Math.PI/1.69);
         return planetWithRings;
     }
     else {
@@ -30,13 +35,25 @@ export function buildARock (size,type,color,cShadow,rShadow,x,y,z,haveRings){
     }
 }
 
-function addRings (innerR,outerR){
+
+//create rings
+function addRings (innerR,outerR,){
     let geo, mater, rings; 
-    geo = new THREE.RingBufferGeometry( 100, 200, 30, 30, 0, 6.3);
-    mater = new THREE.MeshBasicMaterial( { color: 0xc1c1c1, side: THREE.DoubleSide } );
+    geo = new THREE.RingBufferGeometry( 55, 100, 30, 30, 0, 6.3);
+    let ringMap = new THREE.TextureLoader().load(sRings);
+    mater = new THREE.MeshPhysicalMaterial( { map: ringMap, side: THREE.DoubleSide, transparent: true, opacity:0.8} );
     rings = new THREE.Mesh( geo, mater );
     rings.castShadow = false;
     rings.receiveShadow = false;
+    rings.rotateX(Math.PI/1.69);
 
     return rings;
+}
+
+//textures
+function addTexture(imgPath){
+    var img = new THREE.TextureLoader().load(imgPath);
+    var texturedMater = new THREE.MeshLambertMaterial( { map: img } );
+
+    return texturedMater;
 }
